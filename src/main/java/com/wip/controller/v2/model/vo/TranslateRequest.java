4c1dfa6dc4c42b30b18d57361686cd07edcaf810
Jornah Lee <614042560@qq.com>
2020-12-07 00:57:26 +0800

    feat(google-translate)：使用谷歌翻译

---
+++ src/main/java/com/wip/controller/v2/model/vo/TranslateRequest.java
@@ -0,0 +1,52 @@
+package com.wip.controller.v2.model.vo;
+
+import java.util.Arrays;
+
+public class TranslateRequest {
+    private String text;
+    private String sourceLanguage;
+    private String[] targetLanguages;
+    private String secret;
+
+    public String getSecret() {
+        return secret;
+    }
+
+    public void setSecret(String secret) {
+        this.secret = secret;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getSourceLanguage() {
+        return sourceLanguage;
+    }
+
+    public void setSourceLanguage(String sourceLanguage) {
+        this.sourceLanguage = sourceLanguage;
+    }
+
+    public String[] getTargetLanguages() {
+        return targetLanguages;
+    }
+
+    public void setTargetLanguages(String[] targetLanguages) {
+        this.targetLanguages = targetLanguages;
+    }
+
+    @Override
+    public String toString() {
+        return "TranslateRequest{" +
+                "text='" + text + '\'' +
+                ", sourceLanguage='" + sourceLanguage + '\'' +
+                ", targetLanguages=" + Arrays.toString(targetLanguages) +
+                ", secret='" + secret + '\'' +
+                '}';
+    }
+}
